7cccb1f76e922cd82316d2c7b28762fe2d116bf4
Unknown <olmedvedskaya@gmail.com>
2018-03-29 20:06:56 +0300

    Task 2
    My First App: User Interaction

--- myapp.js
+++ myapp.js
@@ -2,10 +2,11 @@ var dataForList = [{"title": "Dashboard"}, {"title": "Users"}, {"title": "Produc
 
 var toolbar = { view:"toolbar", paddingY:1, height:50, elements:[
     { view:"label", label:"My App" },
-    { view:"button", type:"icon", icon:"user", label:"Profile", width: 100, align:"right"}
+    { view:"button", type:"icon", icon:"user", label:"Profile", width: 100, align:"right", popup: "popupForProfile"}
   ]};
   
 var listOnCol = {
+    css:"backgroundList",
     rows:[
         {
             view: "list",
@@ -13,11 +14,10 @@ var listOnCol = {
             data: dataForList,
             autoheight: true,
             width: 300,
-            scroll: false,
-            css: "backgroundList"
+            scroll: false
         },
-        {css:"backgroundList" },
-        {css: "greenText backgroundList", view: "label", label: "<span class='webix_icon fa-check'></span>Connected", align: "center"},
+        { },
+        {css: "greenText", view: "label", label: "<span class='webix_icon fa-check'></span>Connected", align: "center"},
         
     ]
     
@@ -25,8 +25,8 @@ var listOnCol = {
 
 var datatableOnCol = {
     view: "datatable",
+    id: "inputData",
     scrollX: false,
-    autoConfig:true,
     data: small_film_set,
     columns: [
         {id: "title",  header: "Title", fillspace:true},
@@ -37,33 +37,112 @@ var datatableOnCol = {
     ]};            
 
 var formOnCol = {
-    view:"form", scroll:false,
-      width:300,
-      
+    view: "form",
+    width: 300,
+    id: "mainForm",
   	elements:[
-        { type:"section", template:"EDIT FILM" },
-        { view:"text", label:"Title"},
-        { view:"text", label:"Year"},
-        { view:"text", label:"Rating"},
-        { view:"text", label:"Votes"},
+        { type:"section", template:"Edit film" },
+        { view:"text", label:"Title", name:"title", invalidMessage:"Enter title"},
+        { view:"text", label:"Year", name:"year", invalidMessage:"Enter year between 1970 and current"},
+        { view:"text", label:"Rating", name:"rating", invalidMessage:"Enter rating more than 0"},
+        { view:"text", label:"Votes", name:"votes", invalidMessage:"Enter votes less than 100000"},
         
         { margin:5, cols:[
-            { view:"button", label:"Add new", css:"greenButton"},
-            { view:"button", label:"Clear"}
-        ]},
+            { view:"button", label:"Add new", type: "form", 
+                click:function(){
+                    if ($$("mainForm").validate()){                        
+                        var item = $$("mainForm").getValues();
+                        
+                        for (let i in item){
+                            if (item.hasOwnProperty(i)){
+                                item[i] = checkReg(item[i]);
+                            }
+                        }
+                        $$("inputData").add(item);
+                        webix.message("Validation is successful");
+                        
+                    }
+                }
+        
+            },
+            { view:"button", label:"Clear", 
+            click:function(){
+                $$("mainForm").clearValidation();
+                    webix.confirm({
+                        title:"Information",
+                        text:"Clear form?",
+                        callback: function(result){
+                            if (result == true){
+                                $$("mainForm").clear();
+                            }
+                        }
+                  });
+
+            }}
+        ],},
         { }
-  	]           
+      ],
+      rules:{
+        title: webix.rules.isNotEmpty,
+        year: function(value){
+            return value >= 1970 && value <= 2018;
+        },
+        votes: function(value){
+            return value < 10000 && value > 0;
+        },
+        rating: function(value){ 
+            return value > 0;
+        }
+    }           
 };
 
 var footer = {
     view: "template", 
-    height: 40,
+    height: 50,
     css:"textTemplate", 
-    template: "The software is provided by <a href=\"https://webix.com\">https://webix.com</a>. All rights reserved (c)."
+    template: "The software is provided by <a href=\"https://webix.com\" target=\"_blank\">https://webix.com</a>. All rights reserved (c)."
 }
 
 var main = {cols: [ listOnCol, { view: "resizer"}, datatableOnCol, formOnCol]}
 
 webix.ui({
+    view: "popup",
+    id: "popupForProfile",
+    head: "Submenu",
+    width: 300,
+    body: {
+        view: "list",
+        data: [ "Settings", "Log out"],
+        autoheight:true,
+		select:true
+    },
+});
+
+var winMess = webix.ui({
+    view:"window",
+    id:"windowMess",
+    height:100,
+    width:300,
+    position:"center",
+    move:true,
+    head:{
+        view:"toolbar", cols:[
+          {view:"label", label: "Information" },
+          { view:"button", label: 'Close', width: 100, align: 'right', click:"$$('windowMess').close();"}
+        ]
+      }
+      ,
+      body: {view:"label", label: "Clear form?", align: "center" }
+});
+
+
+webix.ui({
     rows: [toolbar, main, footer]
 });
+
+
+function checkReg(str){
+    str = str.replace(/<|>/g, "");
+    return str
+}
+    
